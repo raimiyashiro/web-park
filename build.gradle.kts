d01cd68614cf489eb9c13b775a559773b941fb43
Rai Miyashiro <dev.raimiyashiro@gmail.com>
2022-10-18 06:46:47 -0300

    add spring web, devtools, and mustache

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-mustache")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
